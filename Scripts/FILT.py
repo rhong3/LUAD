d0c22719d1052273943ff513038e48c7780f080f
rh2740 <rh2740@nyu.edu>
2019-06-17 19:53:52 -0400

    6.17.6

---
+++ Scripts/FILT.py
@@ -0,0 +1,31 @@
+"""
+Filter out pos tiles with score less than 0.8
+
+Created on 06/17/2019
+
+@author: RH
+"""
+
+import pandas as pd
+import os
+import sys
+
+dirr = sys.argv[1]  # name of output directory
+File = pd.read_csv('../Results/{}/out/finaldict.csv'.format(dirr), header=0)
+
+pics = []
+
+for index, row in File.iterrows():
+    if row['POS_score'] > 0.8:
+        pics.append(str(row['Num']))
+
+f = []
+for (dirpath, dirnames, filenames) in os.walk('../Results/{}/out/Test_img'.format(dirr)):
+    f.extend(filenames)
+    break
+
+bad = []
+for i in f:
+    if i.split('_')[0] not in pics:
+        os.remove('../Results/{}/out/Test_img/{}'.format(dirr, i))
+
