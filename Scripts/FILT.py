badd8cabc772ff0ee82359d9cfd602cd5a3c91cb
rh2740 <rh2740@nyu.edu>
2019-06-24 10:38:23 -0400

    6.24.1

--- Scripts/FILT.py
+++ Scripts/FILT.py
@@ -11,21 +11,21 @@ import os
 import sys
 
 dirr = sys.argv[1]  # name of output directory
-File = pd.read_csv('../Results/{}/out/finaldict.csv'.format(dirr), header=0)
+File = pd.read_csv('../Results/I3_App/Positive/{}/out/finaldict.csv'.format(dirr), header=0)
 
 pics = []
 
 for index, row in File.iterrows():
-    if row['POS_score'] > 0.8:
+    if row['POS_score'] > 0.95:
         pics.append(str(row['Num']))
 
 f = []
-for (dirpath, dirnames, filenames) in os.walk('../Results/{}/out/Test_img'.format(dirr)):
+for (dirpath, dirnames, filenames) in os.walk('../Results/I3_App/Positive/{}/out/Test_img'.format(dirr)):
     f.extend(filenames)
     break
 
 bad = []
 for i in f:
     if i.split('_')[0] not in pics:
-        os.remove('../Results/{}/out/Test_img/{}'.format(dirr, i))
+        os.remove('../Results/I3_App/Positive/{}/out/Test_img/{}'.format(dirr, i))
 
