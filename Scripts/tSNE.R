cea46b0b58c367b51c5b32ed7abcf64667ec3229
Runyu Hong <30939861+rhong3@users.noreply.github.com>
2019-08-20 12:06:39 -0400

    8.20.1

---
+++ Scripts/tSNE.R
@@ -0,0 +1,73 @@
+## reduce dimensionality of the acitvation layer of model
+## visualize the manifold
+
+# args = commandArgs(trailingOnly=TRUE)
+# input_file=args[1]
+# output_file=args[2]
+# start=args[3]
+# bins=args[4]
+# POS_score=args[5]
+
+input_file='/Users/rh2740/documents/LUAD/Results/I6STK11/out/For_tSNE.csv'
+output_file='/Users/rh2740/documents/LUAD/Results/I6STK11/out/tSNE.csv'
+start=9
+bins=100
+POS_score='POS_score'
+
+library(Rtsne)
+dat = read.table(file=input_file,header=T,sep=',')
+X = as.matrix(dat[,start:dim(dat)[2]])
+res = Rtsne(X, initial_dims=100)
+Y=res$Y
+out_dat = cbind(dat[,1:(start-1)],Y)
+
+dat = cbind(dat,x_bin=cut(dat[,start],bins),
+            y_bin=cut(dat[,(start+1)],bins))
+
+dat = cbind(dat, x_int = as.numeric(dat$x_bin),
+            y_int = as.numeric(dat$y_bin))
+
+colnames(dat)[start:(start+1)]=c('tsne1','tsne2')
+
+dat$True_label=as.factor(dat$True_label)
+dat$slide=as.factor(dat$slide)
+
+write.table(dat, file=output_file, row.names = F, sep=',')
+
+## plot the manifold with probability
+library(ggplot2)
+library(gridExtra)
+
+p1=ggplot(data=dat,aes(x=tsne1,y=tsne2,col=POS_score))+
+  scale_color_gradient2(high='darkorange',mid='white',low='steelblue',midpoint=0.5)+
+  geom_point(alpha=0.2)+
+  #theme(legend.position='bottom')+
+  xlim(-60,60)+
+  ylim(-60,60)
+
+p2=ggplot(data=dat,aes(x=tsne1,y=tsne2))+
+  geom_point(aes(col=True_label),alpha=0.2)+
+  scale_color_manual(values = c('gray70','red'))+
+  #theme(legend.position='bottom')+
+  xlim(-60,60)+
+  ylim(-60,60)
+
+p3=ggplot(data=dat,aes(x=tsne1,y=tsne2))+
+  geom_point(aes(col=slide),alpha=0.2)+
+  theme(legend.position='none')+
+  xlim(-60,60)+
+  ylim(-60,60)
+
+p4=ggplot(data=subset(dat,True_label==1),aes(x=tsne1,y=tsne2))+
+  geom_point(aes(col=slide),alpha=0.2)+
+  theme(legend.position='none')+
+  xlim(-60,60)+
+  ylim(-60,60)
+
+pdf(file='/Users/rh2740/documents/LUAD/Results/I6STK11/out/LUAD_test_manifold.pdf',
+    width=14,height=7)
+
+grid.arrange(p1,p2,nrow=1)
+grid.arrange(p3,p4,nrow=1)
+
+dev.off()
